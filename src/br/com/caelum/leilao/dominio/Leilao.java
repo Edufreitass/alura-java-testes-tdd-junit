e83cab028b3e9ecd2be1d2d578c8e2e031dbdb4c
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-10-28 16:48:02 -0300

    03 Praticando Test-Driven Development (TDD)

--- src/br/com/caelum/leilao/dominio/Leilao.java
+++ src/br/com/caelum/leilao/dominio/Leilao.java
@@ -8,14 +8,34 @@ public class Leilao {
 
 	private String descricao;
 	private List<Lance> lances;
-	
+
 	public Leilao(String descricao) {
 		this.descricao = descricao;
 		this.lances = new ArrayList<Lance>();
 	}
-	
+
 	public void propoe(Lance lance) {
-		lances.add(lance);
+		if (lances.isEmpty() || podeDarLance(lance.getUsuario())) {
+			lances.add(lance);
+		}
+	}
+
+	private boolean podeDarLance(Usuario usuario) {
+		return !ultimoLanceDados().getUsuario().equals(usuario) && qtdDeLancesDo(usuario) < 5;
+	}
+
+	private int qtdDeLancesDo(Usuario usuario) {
+		int total = 0;
+		for (Lance l : lances) {
+			if (l.getUsuario().equals(usuario)) {
+				total++;
+			}
+		}
+		return total;
+	}
+
+	private Lance ultimoLanceDados() {
+		return lances.get(lances.size() - 1);
 	}
 
 	public String getDescricao() {
@@ -26,6 +46,4 @@ public class Leilao {
 		return Collections.unmodifiableList(lances);
 	}
 
-	
-	
 }
---
+++ test/br/com/caelum/leilao/servico/LeilaoTest.java
@@ -0,0 +1,91 @@
+package br.com.caelum.leilao.servico;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+
+public class LeilaoTest {
+
+	@Test
+	public void deveReceberUmLance() {
+		// cenario
+		Leilao leilao = new Leilao("Macbook Pro 15");
+		// Garante que não existe nenhum lance!
+		assertEquals(0, leilao.getLances().size());
+
+		// acao
+		leilao.propoe(new Lance(new Usuario("Steve Jobs"), 2000));
+
+		// validacao
+		assertEquals(1, leilao.getLances().size());
+		assertEquals(2000.0, leilao.getLances().get(0).getValor(), 0.00001);
+	}
+
+	@Test
+	public void deveReceberVariosLances() {
+		// cenario
+		Leilao leilao = new Leilao("Macbook Pro 15");
+
+		// acao
+		leilao.propoe(new Lance(new Usuario("Steve Jobs"), 2000));
+		leilao.propoe(new Lance(new Usuario("Steve Wozniak"), 3000));
+
+		// validacao
+		assertEquals(2, leilao.getLances().size());
+		assertEquals(2000.0, leilao.getLances().get(0).getValor(), 0.00001);
+		assertEquals(3000.0, leilao.getLances().get(1).getValor(), 0.00001);
+	}
+	
+	// Cenário: Uma pessoa não pode dar 2 lances
+	@Test
+	public void naoDeveAceitarDoisLancesSeguidosDoMesmoUsuario() {
+		// cenario
+		Leilao leilao = new Leilao("Macbook Pro 15");
+		Usuario steveJobs = new Usuario("Steve Jobs");
+		
+		// acao
+		leilao.propoe(new Lance(steveJobs, 2000.0));
+		leilao.propoe(new Lance(steveJobs, 3000.0));
+		
+		// validacao
+		assertEquals(1,  leilao.getLances().size());
+		assertEquals(2000.0, leilao.getLances().get(0).getValor(), 0.00001);
+	}
+	
+	
+	// Cenário: Uma pessoa não pode dar mais do que 5 lances
+	@Test
+	public void naoDeveAceitarMaisDoQue5LancesDeUmMesmoUsuario() {
+		// cenario
+		Leilao leilao = new Leilao("Macbook Pro 15");
+		Usuario steveJobs = new Usuario("Steve Jobs");
+		Usuario billGates = new Usuario("Bill Gates");
+		
+		// acao
+		leilao.propoe(new Lance(steveJobs, 2000.0));
+		leilao.propoe(new Lance(billGates, 3000.0));
+		
+		leilao.propoe(new Lance(steveJobs, 4000.0));
+		leilao.propoe(new Lance(billGates, 5000.0));
+		
+		leilao.propoe(new Lance(steveJobs, 6000.0));
+		leilao.propoe(new Lance(billGates, 7000.0));
+		
+		leilao.propoe(new Lance(steveJobs, 8000.0));
+		leilao.propoe(new Lance(billGates, 9000.0));
+		
+		leilao.propoe(new Lance(steveJobs, 10000.0));
+		leilao.propoe(new Lance(billGates, 11000.0));
+
+		// deve ser ignorado
+		leilao.propoe(new Lance(steveJobs, 12000.0));
+		
+		assertEquals(10, leilao.getLances().size());
+		assertEquals(11000.0, leilao.getLances().get(leilao.getLances().size() - 1).getValor(), 0.00001);
+	}
+	
+}
